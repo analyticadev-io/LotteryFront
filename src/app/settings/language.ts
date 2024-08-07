9dbcb4a27b0859844e9145e467c29648fe20043a
miguel000127@gmail.com <miguel000127@gmail.com>
2024-08-07 16:11:18 -0500

    # update language file

---
+++ src/app/settings/language.ts
@@ -0,0 +1,25 @@
+export const language = {
+  //---------------------
+  // INICIO ESPAÑOL
+  //---------------------
+  alert_invalid_login:"Revise su usuario y contraseña e intente de nuevo",
+  title_login:"Iniciar sesion",
+  login_name_imput_form:"Nombre de usuario",
+  login_name_placeholder_imput_form:"Ingresa tu nombre de usuario",
+  login_password_imput_form:"Contraseña",
+  login_password_placeholder_imput_form:"Ingresa tu contraseña",
+
+  //Botones
+  button_login: 'Login',
+  button_sign_up: 'Registro',
+
+
+
+  //---------------------
+  // FIN ESPAÑOL
+  //---------------------
+
+
+
+
+}
