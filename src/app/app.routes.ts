f31c2317d17f1e8c0103dae387d408663b6766ce
miguel000127@gmail.com <miguel000127@gmail.com>
2024-08-07 14:54:00 -0500

    # add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/Login/login/login.component';
+import { HomeComponent } from './pages/Home/home/home.component';
+import { RegistroComponent } from './pages/Registro/registro/registro.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:"",component:LoginComponent},
+  {path:"registro",component:RegistroComponent},
+  {path:"home",component:HomeComponent}
+];
