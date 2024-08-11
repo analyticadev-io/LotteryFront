9d49bd28ef5163597ebba5786fdae8dac7d79e70
miguel000127@gmail.com <miguel000127@gmail.com>
2024-08-11 14:50:48 -0500

    # add app.module.ts dsc: - add app.module.ts file to managa ng zorro modules

---
+++ src/app/app.module.ts
@@ -0,0 +1,87 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { routes } from './app.routes';
+import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+//Material components
+import {MatButtonModule} from '@angular/material/button';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { NativeDateModule } from '@angular/material/core';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+
+
+//ReactiveForms
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+//Solicitudes Http
+import { HttpClientModule, provideHttpClient } from '@angular/common/http';
+
+//dayJS
+import * as dayjs from 'dayjs';
+
+
+
+//NG Zorro
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en'
+import { NzDrawerModule } from 'ng-zorro-antd/drawer';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+
+
+//Components
+import { HomeComponent } from './pages/Home/home/home.component';
+import { LoginComponent } from './pages/Login/login/login.component';
+import { RegistroComponent } from './pages/Registro/registro/registro.component';
+
+
+registerLocaleData(en);
+
+
+@NgModule({
+  declarations: [
+    //AppComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    //Material
+    MatButtonModule,MatPaginatorModule,MatTableModule,ReactiveFormsModule,
+    MatInputModule,MatSelectModule,MatDatepickerModule,NativeDateModule,
+    MatSnackBarModule,MatIconModule,MatDialogModule,MatGridListModule,FormsModule,
+    MatCardModule,NzLayoutModule,
+
+    //Http
+    HttpClientModule,
+
+
+    //NGZORRO
+    NzDrawerModule,NzIconModule,NzButtonModule,
+
+    //componentes
+    HomeComponent,
+    LoginComponent,
+    RegistroComponent
+
+  ],
+  providers: [
+    provideAnimationsAsync(),
+    { provide: NZ_I18N, useValue: en_US },
+    provideHttpClient()
+  ],
+  //bootstrap: [AppComponent]
+})
+export class AppModule { }
