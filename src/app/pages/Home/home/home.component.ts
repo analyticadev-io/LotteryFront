d6675d2262336f64c9f53142600cf27793de99ec
miguel000127@gmail.com <miguel000127@gmail.com>
2024-08-11 14:54:49 -0500

    # add home component dsc: - add home component to manage homes view

--- src/app/pages/Home/home/home.component.ts
+++ src/app/pages/Home/home/home.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 
+import { NzDrawerModule } from 'ng-zorro-antd/drawer';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NzDrawerModule,NzButtonModule,NzLayoutModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
+  visible = false;
+
+  open(): void {
+    this.visible = true;
+  }
+
+  close(): void {
+    this.visible = false;
+  }
 }
