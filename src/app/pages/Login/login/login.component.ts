d9685a6274110aadea9f2e8d8a1a6e934fb8408b
miguel000127@gmail.com <miguel000127@gmail.com>
2024-08-11 10:46:54 -0500

    # update login function
    dsc:
    - update login function to use new response interface

--- src/app/pages/Login/login/login.component.ts
+++ src/app/pages/Login/login/login.component.ts
@@ -1,20 +1,23 @@
-
 import { Router } from '@angular/router';
 import { AccesoService } from './../../../services/Acceso/acceso.service';
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Login } from '../../../interfaces/Login';
 import { language } from '../../../settings/language';
+import { appsettings } from '../../../settings/appsettings';
 
 import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatCardModule,MatFormFieldModule,MatInputModule,MatButtonModule,ReactiveFormsModule],
+  imports: [MatCardModule,MatFormFieldModule,MatInputModule,MatButtonModule,ReactiveFormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
@@ -23,7 +26,7 @@ export class LoginComponent {
   private router = inject(Router);
   private formBuild = inject(FormBuilder);
   public language: any=language;
-
+  private _snackBar = inject(MatSnackBar);
 
   public formLogin: FormGroup = this.formBuild.group({
     NombreUsuario: ['', Validators.required],
@@ -39,19 +42,27 @@ export class LoginComponent {
     this.AccesoService.Login(obj).subscribe({
       next:(data)=>{
         if(data.isSuccess){
-          localStorage.setItem("token",data.token)
+          const token = data.token.token;
+          localStorage.setItem("token",token);
           this.router.navigate(['home'])
         }else{
-          alert(language.alert_invalid_login)
+          this.openSnackBar(this.language.alert_invalid_login);
         }
       },error:(error)=>{
-        console.log(error)
+        console.error(error)
+        this.openSnackBar(this.language.alert_invalid_login);
       }
     })
   }
 
   Registro(){
     this.router.navigate(['registro'])
+  }
+
+  openSnackBar(message: string) {
+    this._snackBar.open(message, language.button_action_close, {
+      duration: appsettings.login_alert_duration_in_ss * 1000,
+    });
   }
 
 
