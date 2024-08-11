f9cc4c9dc0a9d85672473072ab288bdd7e56bd2c
miguel000127@gmail.com <miguel000127@gmail.com>
2024-08-11 10:44:10 -0500

    # update language constants file

--- src/app/settings/language.ts
+++ src/app/settings/language.ts
@@ -2,16 +2,37 @@ export const language = {
   //---------------------
   // INICIO ESPAÑOL
   //---------------------
-  alert_invalid_login:"Revise su usuario y contraseña e intente de nuevo",
+
+
+  /**
+   * Login
+   */
   title_login:"Iniciar sesion",
+  alert_invalid_login:"Revise su usuario y contraseña e intente de nuevo",
   login_name_imput_form:"Nombre de usuario",
+  name_required:"Nombre de usuario requerido",
   login_name_placeholder_imput_form:"Ingresa tu nombre de usuario",
   login_password_imput_form:"Contraseña",
   login_password_placeholder_imput_form:"Ingresa tu contraseña",
+  password_required:"Contraseña requerida",
+
+  /**
+   * Registro
+   */
+  alert_invalid_registro:"Error al registrarse",
+  alert_valid_registro:"Usuario registrado, inicia sesion",
+  register_email_input_form:'Email',
+  register_email_placegolder_input_form:'Ingresa tu email',
+  email_required:"Email requerido",
+  register_fullname_input_form:'Nombre completo',
+  register_fullname_placegolder_input_form:'Ingresa tu nombre completo',
+  fullname_required:"Nombre completo requerido",
 
   //Botones
   button_login: 'Login',
   button_sign_up: 'Registro',
+  button_action_sign_up: 'Registrarse',
+  button_action_close: 'Cerrar',
 
 
 
