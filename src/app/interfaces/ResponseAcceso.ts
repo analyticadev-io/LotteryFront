fdb5a698076cef87741f3506bc3912f2e4ec3773
miguel000127@gmail.com <miguel000127@gmail.com>
2024-08-11 10:46:06 -0500

    # update response acceso
    dsc:
    - Update the Access Response interface to be similar to the response obtained from the server

--- src/app/interfaces/ResponseAcceso.ts
+++ src/app/interfaces/ResponseAcceso.ts
@@ -1,4 +1,11 @@
-export interface ResponseAcceso{
-  isSuccess:boolean,
-  token:string
+import { Usuario } from "./Usuario";
+
+export interface Token {
+  token: string;
+  usuario: Usuario;
+}
+
+export interface ResponseAcceso {
+  isSuccess: boolean;
+  token: Token;
 }
