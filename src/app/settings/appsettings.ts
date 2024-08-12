48d060172223e66d4338400e7615c159370db7af
miguel000127@gmail.com <miguel000127@gmail.com>
2024-08-12 16:20:46 -0500

    # update app settings

--- src/app/settings/appsettings.ts
+++ src/app/settings/appsettings.ts
@@ -1,4 +1,7 @@
 export const appsettings = {
   apiBaseUrl:"https://localhost:7046/api/",
-  login_alert_duration_in_ss :5
+  login_alert_duration_in_ss :5,
+  cryptoJs_secure_key:"IubfLjdDdwKVE0p",
+  cryptoJs_secure_MD5_crypted_key:"5b05196eb7aa7f487ad6de85828e1b09",
+
 }
