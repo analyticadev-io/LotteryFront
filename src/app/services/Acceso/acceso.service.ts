ca55efe83c8de8022c61242a769571817db872b2
miguel000127@gmail.com <miguel000127@gmail.com>
2024-08-17 22:27:05 -0500

    [update] acceso service

--- src/app/services/Acceso/acceso.service.ts
+++ src/app/services/Acceso/acceso.service.ts
@@ -5,6 +5,8 @@ import { Usuario } from '../../interfaces/Usuario';
 import { Observable } from 'rxjs';
 import { ResponseAcceso } from '../../interfaces/ResponseAcceso';
 import { Login } from '../../interfaces/Login';
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +15,7 @@ export class AccesoService {
 
   private http = inject(HttpClient)
   private baseUrl:string = appsettings.apiBaseUrl
-  constructor() { }
+  constructor(private cookieService:CookieService, private router: Router) { }
 
   Registro(objeto:Usuario): Observable<Usuario>{
     return this.http.post<Usuario>(`${this.baseUrl}Acceso/Registro`,objeto)
@@ -21,6 +23,12 @@ export class AccesoService {
 
   Login(objeto:Login): Observable<ResponseAcceso>{
     return this.http.post<ResponseAcceso>(`${this.baseUrl}Acceso/Login`,objeto)
+  }
+
+  LogOut(){
+    this.cookieService.deleteAll();
+    localStorage.clear();
+    this.router.navigate(['']);
   }
 
 }
