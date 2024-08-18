910f63c220c8a0c6e76f8844ce85aea74e1b890c
miguel000127@gmail.com <miguel000127@gmail.com>
2024-08-17 22:23:55 -0500

    [update] menu options service

--- src/app/services/MenuOptions/menu-options.service.ts
+++ src/app/services/MenuOptions/menu-options.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { MenuOptions } from '../../interfaces/MenuOptions';
 import { BehaviorSubject } from 'rxjs';
 import { language } from '../../settings/language';
+import { ModulesService } from '../Modules/modules.service';
 
 @Injectable({
   providedIn: 'root'
@@ -20,31 +21,63 @@ export class MenuOptionsService {
    *
    */
 
-  private menuItemsSource = new BehaviorSubject<MenuOptions[]>([
-    { name: language.menu_option_display_name_Roles,
-      module_name: language.menu_option_module_name_Roles,
-      visibilityStatus: false,
-      icon:"lock"
-    }
-  ]);
+    public newMenu= new BehaviorSubject<MenuOptions[]>([])
 
-  constructor() { }
+  // private menuItemsSource = new BehaviorSubject<MenuOptions[]>([
+  //   { name: language.menu_option_display_name_Roles,
+  //     module_name: language.menu_option_module_name_Roles,
+  //     visibilityStatus: false,
+  //     icon:"lock"
+  //   },
+  //   { name: language.menu_option_display_name_Modules,
+  //     module_name: language.menu_option_module_name_Modules,
+  //     visibilityStatus: false,
+  //     icon:"unlock"
+  //   },
+  //   { name: "Modulo de prueba",
+  //     module_name: "nuevaPrueba",
+  //     visibilityStatus: false,
+  //     icon:"small-dash"
+  //   },
+  // ]);
+
+  constructor(private moduleService: ModulesService) {
+    this.getDBModules();
+   }
 
   private activeComponentSource = new BehaviorSubject<string>('');
 
-  menuItems$ = this.menuItemsSource.asObservable();
+  menuItems$ = this.newMenu.asObservable();
   activeComponent$ = this.activeComponentSource.asObservable();
 
   toggleVisibility(itemName: string): void {
 
-    const updatedMenuItems = this.menuItemsSource.value.map(item => {
+    const updatedMenuItems = this.newMenu.value.map(item => {
       item.visibilityStatus = (item.module_name === itemName) ? !item.visibilityStatus : false;
       return item;
     });
-    this.menuItemsSource.next(updatedMenuItems);
+    this.newMenu.next(updatedMenuItems);
 
     const activeItem = updatedMenuItems.find(item => item.visibilityStatus);
     this.activeComponentSource.next(activeItem ? activeItem.module_name : '');
+  }
+
+  getDBModules(){
+    this.moduleService.GetModules().subscribe({
+      next:(data)=>{
+        const menuObj: MenuOptions[] = data.map(module => ({
+          id:module.idModule,
+          name: module.name, // Ajusta estos campos según el formato de los datos recibidos
+          module_name: module.module_name,
+          visibilityStatus: false, // Inicializado en false o según tus necesidades
+          icon: module.icon || "no-icon" // Usa un valor predeterminado si no se proporciona
+        }));
+        this.newMenu.next(menuObj);
+        console.log('servico menus',menuObj);
+      },error:(error)=>{
+
+      }
+    });
   }
 
 }
