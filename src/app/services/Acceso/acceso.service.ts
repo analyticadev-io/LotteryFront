cd3cb5a31bb6f656b322597377c4e96b298c66c7
miguel000127@gmail.com <miguel000127@gmail.com>
2024-08-20 23:13:28 -0500

    [HU0002] Generate Synchronous Encryption Service for Data Transmission dsc: - add encrypt/decrypt methods

--- src/app/services/Acceso/acceso.service.ts
+++ src/app/services/Acceso/acceso.service.ts
@@ -8,6 +8,7 @@ import { Login } from '../../interfaces/Login';
 import { CookieService } from 'ngx-cookie-service';
 import { Router } from '@angular/router';
 import { environment } from '../../../environments/environment';
+import * as CryptoJS from 'crypto-js';
 
 @Injectable({
   providedIn: 'root'
@@ -18,12 +19,41 @@ export class AccesoService {
   private baseUrl:string = environment.apiUrl
   constructor(private cookieService:CookieService, private router: Router) { }
 
+
+  encrypt(data: string): string {
+    const key = CryptoJS.enc.Hex.parse('5b05196eb7aa7f487ad6de85828e1b09');
+    const iv = CryptoJS.enc.Hex.parse('00000000000000000000000000000000');
+    const encrypted = CryptoJS.AES.encrypt(data, key, {
+        iv: iv,
+        mode: CryptoJS.mode.CBC,
+        padding: CryptoJS.pad.Pkcs7
+    });
+    return encrypted.toString();
+}
+
+decrypt(encryptedData: string): string {
+  const key = CryptoJS.enc.Hex.parse('5b05196eb7aa7f487ad6de85828e1b09');
+  const iv = CryptoJS.enc.Hex.parse('00000000000000000000000000000000');
+  const decrypted = CryptoJS.AES.decrypt(encryptedData, key, {
+      iv: iv,
+      mode: CryptoJS.mode.CBC,
+      padding: CryptoJS.pad.Pkcs7
+  });
+  return decrypted.toString(CryptoJS.enc.Utf8);
+}
+
+
   Registro(objeto:Usuario): Observable<Usuario>{
     return this.http.post<Usuario>(`${this.baseUrl}Acceso/Registro`,objeto)
   }
 
   Login(objeto:Login): Observable<ResponseAcceso>{
-    return this.http.post<ResponseAcceso>(`${this.baseUrl}Acceso/Login`,objeto)
+    const encryptedPassword = this.encrypt(objeto.contrasena);
+    // Reemplaza la contraseña en el objeto con la versión encriptada
+    const loginData = { ...objeto, contrasena: encryptedPassword };
+    //console.log(encryptedPassword);
+    //console.log(loginData);
+    return this.http.post<ResponseAcceso>(`${this.baseUrl}Acceso/Login`,loginData)
   }
 
   LogOut(){
