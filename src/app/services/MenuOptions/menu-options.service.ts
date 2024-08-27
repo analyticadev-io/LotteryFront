8db36d4bce76c66d6e64ad6cfb69c2120c5ee847
miguel000127@gmail.com <miguel000127@gmail.com>
2024-08-26 19:36:06 -0500

    [update] menu options service

--- src/app/services/MenuOptions/menu-options.service.ts
+++ src/app/services/MenuOptions/menu-options.service.ts
@@ -12,7 +12,10 @@ export class MenuOptionsService {
   public newMenu = new BehaviorSubject<MenuOptions[]>([]);
   private activeComponentSource = new BehaviorSubject<string>('');
 
-  constructor(private moduleService: ModulesService, private _encryptservice:EncryptService) {
+  constructor(
+    private moduleService: ModulesService,
+    private _encryptservice: EncryptService
+  ) {
     this.getDBModules();
     //console.log('estoy en el servicio opciones');
   }
@@ -22,37 +25,37 @@ export class MenuOptionsService {
   activeComponent$ = this.activeComponentSource.asObservable();
 
   // Método para alternar la visibilidad de un ítem del menú
-  // toggleVisibility(itemName: string): void {
-  //   const updatedMenuItems = this.newMenu.value.map((item) => {
-  //     item.visibilityStatus =
-  //       item.module_name === itemName ? !item.visibilityStatus : "false";
-  //     return item;
-  //   });
-  //   this.newMenu.next(updatedMenuItems);
-
-  //   const activeItem = updatedMenuItems.find((item) => item.visibilityStatus);
-  //   this.activeComponentSource.next(activeItem ? activeItem.module_name : '');
-  // }
-
   toggleVisibility(itemName: string): void {
+    // Primero, desactiva cualquier opción que esté actualmente visible
     const updatedMenuItems = this.newMenu.value.map((item) => {
-      item.visibilityStatus =
-        item.module_name === itemName
-          ? (item.visibilityStatus === "true" ? "false" : "true") // Cambia entre "true" y "false"
-          : item.visibilityStatus;
+      if (item.visibilityStatus === 'true') {
+        item.visibilityStatus = 'false';
+      }
       return item;
     });
-    this.newMenu.next(updatedMenuItems);
 
-    const activeItem = updatedMenuItems.find((item) => item.visibilityStatus === "true");
+    // Luego, activa la opción seleccionada
+    const newUpdatedMenuItems = updatedMenuItems.map((item) => {
+      if (item.module_name === itemName) {
+        item.visibilityStatus = 'true';
+      }
+      return item;
+    });
+
+    // Actualiza el estado del menú con los cambios
+    this.newMenu.next(newUpdatedMenuItems);
+
+    // Actualiza el componente activo
+    const activeItem = newUpdatedMenuItems.find(
+      (item) => item.visibilityStatus === 'true'
+    );
     this.activeComponentSource.next(activeItem ? activeItem.module_name : '');
   }
-
 
   getDBModules() {
     this.moduleService.GetModules().subscribe({
       next: (data) => {
-        if(data.response){
+        if (data.response) {
           let decryptedResponse = this._encryptservice.decrypt(data.response);
           //console.log(decryptedResponse)
           let objMenu = JSON.parse(decryptedResponse) as MenuOptions[];
@@ -61,15 +64,14 @@ export class MenuOptionsService {
             id: module.IdModule,
             Name: module.Name, // Ajusta estos campos según el formato de los datos recibidos
             module_name: module.module_name,
-            visibilityStatus: "false",
+            visibilityStatus: 'false',
             icon: module.icon || 'no-icon',
           }));
           this.newMenu.next(menuObj);
           //console.log('servico menus', menuObj);
-        }else{
-          console.error("ERROR: obtaninig response menu-options.service.ts");
+        } else {
+          console.error('ERROR: obtaninig modules response menu-options.service.ts');
         }
-
       },
       error: (error) => {
         console.error(error);
@@ -80,7 +82,7 @@ export class MenuOptionsService {
   // Método para desactivar todas las opciones del menú
   toggleToOffAllOptions() {
     const updatedMenuItems = this.newMenu.value.map((item) => {
-      item.visibilityStatus = "false";
+      item.visibilityStatus = 'false';
       return item;
     });
     this.newMenu.next(updatedMenuItems);
